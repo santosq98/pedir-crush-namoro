966a29b5cbb038be8d31904ceca567d4988c5339
Santosq98 <gabriel.santosq@gmail.com>
2022-10-18 10:36:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# PedirCrushNamoro
+Técnica imperdível para pedir Crush em Namoro!
+
+Clique na imagem abaixo para ver funcionando:<br>
+<a href="https://www.instagram.com/reel/CjLN4bgjTvK/?utm_source=ig_web_copy_link">
+  <img src="https://user-images.githubusercontent.com/112261177/193423320-3b8c02e6-6040-4e52-a446-e5e092af268e.png">
+</a>
---
+++ buttonHover.js
@@ -0,0 +1,13 @@
+$('button').on('mouseover', function() {
+    $(this).animate({
+      'top': Math.random() * document.
+                getElementById('content').
+                clientHeight + "px",
+      'left': Math.random() * document.
+                getElementById('content').
+                clientWidth + "px"
+    }, "medium");
+  })
+
+
+  
